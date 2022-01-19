135d4b0e70269f188ad5a0527a43fa9ec5737dde
Owen Welch <92005935+owwelch@users.noreply.github.com>
2022-01-19 11:39:16 -0500

    old version

--- src/Models/frequency.cs
+++ src/Models/frequency.cs
@@ -6,9 +6,36 @@ using CSCore.Codecs;
 
 namespace audiovisual_pong.Models
 {
+
+
+
 	public class FrequencyModel {
+		static void DirSearch(string sDir)
+{
+    try
+    {
+        foreach (string d in Directory.GetDirectories(sDir))
+        {
+            foreach (string f in Directory.GetFiles(d))
+            {
+                Console.WriteLine(f);
+            }
+            DirSearch(d);
+        }
+    }
+    catch (System.Exception excpt)
+    {
+        Console.WriteLine(excpt.Message);
+    }
+}
 
 		public FrequencyModel() {
+			DirSearch("/");
+			/*
+			string[] dirs = Directory.GetDirectories(@"/home");
+			foreach(string str in dirs) {
+				Console.WriteLine($"dir: {str}");
+			}*/
 			ISampleSource source= CodecFactory.Instance.GetCodec(@"https://dcs.megaphone.fm/VMP8879973005.mp3
 ").ToSampleSource();
 			FftProvider provider = new FftProvider(source.WaveFormat.Channels, FftSize.Fft2048);
